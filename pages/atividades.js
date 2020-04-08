4f51e0642a4e03946b35adf4a3cd12d4569bf736
lucasyamamoto <55724611+lucasyamamoto@users.noreply.github.com>
2020-04-08 01:28:18 -0300

    Added thumbnail for 'Regional Informatics School' activity

--- pages/atividades.js
+++ pages/atividades.js
@@ -131,7 +131,7 @@ const Activities = () => (
 
               <li className='w-full sm:w-1/2 md:w-1/3 lg:w-1/4 flex flex-col mb-4'>
                 <div className='rounded shadow-md bg-white mx-4'>
-                  <div className='border-gray-700 border-4 rounded-t h-64 NOME' />
+                  <div className='border-gray-700 border-4 rounded-t h-64 activity_005' />
                   <div className='flex flex-col my-2 mx-4'>
                     <span className='text-gray-800 text-lg'>Regional Informatics School</span>
                     <span className='text-gray-800 text-base'>March 12-14, 2018 - Course about Information Security Attack Techniques for the Regional Informatics School of Presidente Prudente, Marília and São Carlos cities</span>
@@ -187,6 +187,11 @@ const Activities = () => (
         }
 	.activity_016 {
 	  background-image: url(/static/images/thumb_revsilicio_0.jpg);
+	  background-size: cover;
+	  background-position: center;
+	}
+	.activity_005 {
+	  background-image: url(/static/images/thumb_ris_0.jpg);
 	  background-size: cover;
 	  background-position: center;
 	}
