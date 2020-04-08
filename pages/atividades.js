4b94f349c3e9759f54936c3969e119abe5ce7d35
lucasyamamoto <55724611+lucasyamamoto@users.noreply.github.com>
2020-04-08 00:55:25 -0300

    Added thumbnail for 'BlueBorne Presentation for Roadsec' activity

--- pages/atividades.js
+++ pages/atividades.js
@@ -151,7 +151,7 @@ const Activities = () => (
 
               <li className='w-full sm:w-1/2 md:w-1/3 lg:w-1/4 flex flex-col mb-4'>
                 <div className='rounded shadow-md bg-white mx-4'>
-                  <div className='border-gray-700 border-4 rounded-t h-64 NOME' />
+                  <div className='border-gray-700 border-4 rounded-t h-64 activity_003' />
                   <div className='flex flex-col my-2 mx-4'>
                     <span className='text-gray-800 text-lg'>BlueBorne Presentation for Roadsec</span>
                     <span className='text-gray-800 text-base'>November 11, 2017 - Demonstration about BlueBorne attack, witch exploits a Bluetooth vulnerability. The presentation was in Roadsec, the biggest security event on Latin America</span>
@@ -190,13 +190,18 @@ const Activities = () => (
 	  background-size: cover;
 	  background-position: center;
 	}
-	.activity_001 {
-	  background-image: url(/static/images/thumb_senc2017_0.jpg);
+	.activity_003 {
+	  background-image: url(/static/images/thumb_blueborne_0.jpg);
 	  background-size: cover;
 	  background-position: center;
 	}
 	.activity_002 {
 	  background-image: url(/static/images/thumb_unesp_0.jpg);
+	  background-size: cover;
+	  background-position: center;
+	}
+	.activity_001 {
+	  background-image: url(/static/images/thumb_senc2017_0.jpg);
 	  background-size: cover;
 	  background-position: center;
 	}
