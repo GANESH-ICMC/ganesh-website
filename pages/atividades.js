5c07852028212a720a8459688a5598fa07cdd8ae
lucasyamamoto <55724611+lucasyamamoto@users.noreply.github.com>
2020-04-08 01:39:18 -0300

    Added thumbnail for 'Pint of Science Presentation' acitivity

--- pages/atividades.js
+++ pages/atividades.js
@@ -110,7 +110,7 @@ const Activities = () => (
 
               <li className='w-full sm:w-1/2 md:w-1/3 lg:w-1/4 flex flex-col mb-4'>
                 <div className='rounded shadow-md bg-white mx-4'>
-                  <div className='border-gray-700 border-4 rounded-t h-64 NOME' />
+                  <div className='border-gray-700 border-4 rounded-t h-64 activity_007' />
                   <div className='flex flex-col my-2 mx-4'>
                     <span className='text-gray-800 text-lg'>Pint of Science Presentation</span>
                     <span className='text-gray-800 text-base'>May 15, 2018 - Presentation with the theme "Help: I was hacked and the drone vanished!" in the Pint of Science event. Pint of Science is a festival that aims to deliver talks on science research in an accessible format to the public, mainly across bars, pubs, cafes and other public spaces
@@ -187,6 +187,11 @@ const Activities = () => (
         }
 	.activity_016 {
 	  background-image: url(/static/images/thumb_revsilicio_0.jpg);
+	  background-size: cover;
+	  background-position: center;
+	}
+	.activity_007 {
+	  background-image: url(/static/images/thumb_pintofscience_0.jpg);
 	  background-size: cover;
 	  background-position: center;
 	}
