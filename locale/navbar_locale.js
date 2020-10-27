d219523b81702121b35bd0f813bcf1ec43e673d2
Es7evam <estevam.arantes@gmail.com>
2020-10-27 14:46:52 -0300

    Add navbar translation (both mobile and pc)

---
+++ locale/navbar_locale.js
@@ -0,0 +1,43 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+
+
+const resources = {
+  en: {
+    translation: {
+      "aboutus":    "About Us",
+      "institute":  "Our Institute",
+      "university": "Our University",
+      "areas":      "Areas",
+      "activities": "Activities",
+      "news":       "News",
+      "contact":    "Contact"
+    }
+  },
+  br: {
+    translation: {
+      "aboutus":    "Sobre Nós",
+      "institute":  "Nosso Inssituto",
+      "university": "Nossa Universidade",
+      "areas":      "Frentes",
+      "activities": "Atividades",
+      "news":       "Notícias",
+      "contact":    "Contato"
+    }
+  }
+};
+
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .init({
+    resources,
+    lng: "en",
+
+    keySeparator: false, // we do not use keys in form messages.welcome
+
+    interpolation: {
+      escapeValue: true // no xss here :c
+    }
+  });
+
+export default i18n;
