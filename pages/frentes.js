0f14d2c928e68635aff034125e4f1850c53b5b63
Van Loon <gabriel.vl.rojas@gmail.com>
2021-05-18 21:26:13 -0300

    fix: Frentes espaçamento

--- pages/frentes.js
+++ pages/frentes.js
@@ -18,10 +18,10 @@ const Fronts = ({t}) => (
     <div className='flex justify-center items-center bg-fixed bg-cover bg-center container-top shadow-md'>
         <h1 className='text-4xl text-center text-white'><Trans i18nKey="areas:title">Areas</Trans></h1>
       </div>
-      <div className='container mx-auto px-4 py-8'>
+      <div className='container mx-auto px-4 py-4'>
         <ul className='flex flex-row flex-wrap my-4'>
-          <li className='w-full sm:w-1/2 md:w-1/3 lg:w-1/4 flex flex-col mb-4'>
-            <div className='flex-auto rounded shadow-md bg-white mx-4'>
+          <li className='w-full sm:w-1/2 md:w-1/3 lg:w-1/4 flex flex-col p-4'>
+            <div className='flex-auto rounded shadow-md bg-white'>
               <div className='border-gray-700 border-4 rounded-t h-64 cryptography' />
               <div className='flex flex-col my-2 mx-4'>
                 <span className='text-gray-800 text-lg'><b>{t('areas:tcripto')}</b></span>
@@ -29,8 +29,8 @@ const Fronts = ({t}) => (
               </div>
             </div>
           </li>
-          <li className='w-full sm:w-1/2 md:w-1/3 lg:w-1/4 flex flex-col mb-4'>
-            <div className='flex-auto rounded shadow-md bg-white mx-4'>
+          <li className='w-full sm:w-1/2 md:w-1/3 lg:w-1/4 flex flex-col p-4'>
+            <div className='flex-auto rounded shadow-md bg-white'>
               <div className='border-gray-700 border-4 rounded-t h-64 networking' />
               <div className='flex flex-col my-2 mx-4'>
                 <span className='text-gray-800 text-lg'><b>{t('areas:tnetwork')}</b></span>
@@ -38,8 +38,8 @@ const Fronts = ({t}) => (
               </div>
             </div>
           </li>
-          <li className='w-full sm:w-1/2 md:w-1/3 lg:w-1/4 flex flex-col mb-4'>
-            <div className='flex-auto rounded shadow-md bg-white mx-4'>
+          <li className='w-full sm:w-1/2 md:w-1/3 lg:w-1/4 flex flex-col p-4'>
+            <div className='flex-auto rounded shadow-md bg-white'>
               <div className='border-gray-700 border-4 rounded-t h-64 reversing'/>
               <div className='flex flex-col my-2 mx-4'>
                 <span className='text-gray-800 text-lg'><b>{t('areas:trev')}</b></span>
@@ -49,8 +49,8 @@ const Fronts = ({t}) => (
               </div>
             </div>
           </li>
-          <li className='w-full sm:w-1/2 md:w-1/3 lg:w-1/4 flex flex-col mb-4'>
-            <div className='flex-auto rounded shadow-md bg-white mx-4'>
+          <li className='w-full sm:w-1/2 md:w-1/3 lg:w-1/4 flex flex-col p-4'>
+            <div className='flex-auto rounded shadow-md bg-white'>
               <div className='border-gray-700 border-4 rounded-t h-64 web' />
               <div className='flex flex-col my-2 mx-4'>
                 <span className='text-gray-800 text-lg'><b>{t('areas:tweb')}</b></span>
@@ -60,8 +60,8 @@ const Fronts = ({t}) => (
               </div>
             </div>
           </li>
-          <li className='w-full sm:w-1/2 md:w-1/3 lg:w-1/4 flex flex-col mb-4'>
-            <div className='flex-auto rounded shadow-md bg-white mx-4'>
+          <li className='w-full sm:w-1/2 md:w-1/3 lg:w-1/4 flex flex-col p-4'>
+            <div className='flex-auto rounded shadow-md bg-white'>
               <div className='border-gray-700 border-4 rounded-t h-64 hardware' />
               <div className='flex flex-col my-2 mx-4'>
                 <span className='text-gray-800 text-lg'><b>{t('areas:thardware')}</b></span>
