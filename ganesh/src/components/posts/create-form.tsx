8db90097e17993d08c576c3cf1ea26c9b5ee9ee4
shogoshima <shogoshima@usp.br>
2024-10-13 14:13:00 -0300

    ajustes finais: trocando nome para ganesh e ganesh-website

--- ganesh/src/components/posts/create-form.tsx
+++ ganesh/src/components/posts/create-form.tsx
@@ -102,14 +102,14 @@ export default function Form({ authors }: { authors: Author[] }) {
           </label>
           <div className="relative mt-2 rounded-md">
             <div className="relative">
-              <input
+              <textarea
                 id="summary"
                 name="summary"
-                type="text"
+                rows={4} 
                 placeholder="Sumário"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               />
-              <Bars4Icon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+              <Bars4Icon className="pointer-events-none absolute left-3 top-5 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
           </div>
         </div>
